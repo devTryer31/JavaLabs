e74a8f979b6e90901bbd464c56a560532de5170c
Валера Герасимов <valera.gs@bk.ru>
2022-01-06 16:33:46 +0300

    CallBack progress system added.

--- lab_9_IndividualTask/src/ITmain.java
+++ lab_9_IndividualTask/src/ITmain.java
@@ -1,15 +1,29 @@
 import DirectoriesUtils.HashSumChecker;
 import Models.HTTP.MultiThreadDownloader;
 
+import java.awt.font.NumericShaper;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.security.NoSuchAlgorithmException;
+import java.util.Map;
 
 public class ITmain {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         MultiThreadDownloader downloader = new MultiThreadDownloader(30);
+
+        downloader.SetProgressUpdater((n) -> {
+            try {
+                Thread.sleep(100L);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            var pair = (Map.Entry<Integer, Integer>) n;
+            System.out.println("Progress on " + pair.getKey() + " is: " + pair.getValue());
+            return true;
+        });
+
         try {
-            downloader.DownloadFileThreading("https://speedtest.selectel.ru/100MB","D:\\Temp");
+            downloader.DownloadFileThreading("https://speedtest.selectel.ru/100MB", "D:\\Temp");
             System.out.println(HashSumChecker.GetHashSum("D:\\Temp\\100MB", "MD5"));
             System.out.println(HashSumChecker.GetHashSum("D:\\Temp\\100MB_2", "MD5"));
         } catch (URISyntaxException e) {
@@ -22,5 +36,4 @@ public class ITmain {
             e.printStackTrace();
         }
     }
-
 }
--- lab_9_IndividualTask/src/Models/HTTP/MultiThreadDownloader.java
+++ lab_9_IndividualTask/src/Models/HTTP/MultiThreadDownloader.java
@@ -8,13 +8,17 @@ import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.AbstractMap;
+import java.util.Map;
+import java.util.function.Function;
 
 public class MultiThreadDownloader implements WebDownloadClient {
 
     private final DownloadHttpClient _http_downloader;
     private int _response_delay_sec = 10;
     private final int _current_threads_count = 6;
-    private Thread[] _threads_pool = new Thread[_current_threads_count];
+    private final Thread[] _threads_pool = new Thread[_current_threads_count];
+    private Function<Map.Entry<Integer, Integer>, Boolean> _progress_updater = null;
 
     public MultiThreadDownloader(int response_delay_sec) {
         _response_delay_sec = response_delay_sec;
@@ -25,12 +29,13 @@ public class MultiThreadDownloader implements WebDownloadClient {
     public boolean DownloadFileThreading(String URI, String folder_path) throws URISyntaxException, IOException, InterruptedException {
         //Getting the filename.
         final var tmp_arr = URLDecoder.decode(URI, StandardCharsets.UTF_8.toString()).split("/");
-        final String file_name = tmp_arr[tmp_arr.length-1];
+        final String file_name = tmp_arr[tmp_arr.length - 1];
 
         //Build a correct file path.
         final String file_path = folder_path + "\\" + file_name;
 
         int bytes_len = (int) _http_downloader.GetContentSize(URI);
+
         int part_size = bytes_len / _current_threads_count;
 
         int current_thread_id = 0;
@@ -40,28 +45,36 @@ public class MultiThreadDownloader implements WebDownloadClient {
         //Object mutex = new Object();
         byte[] file_bytes = new byte[bytes_len];
 
-        while(current_thread_id != _current_threads_count){
+        while (current_thread_id != _current_threads_count) {
 
             //For protect threads.
             int finalLeft_pos = left_pos, finalRight_pos = right_pos;
-            _threads_pool[current_thread_id] = new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    try {
-                        var response = new DownloadHttpClient(_http_downloader.getResponseDelay())
-                                .DownloadFilePart(URI, finalLeft_pos, finalRight_pos);
+            int thread_id = current_thread_id;
+            _threads_pool[current_thread_id] = new Thread(() -> {
+                try {
+                    var response = new DownloadHttpClient(_http_downloader.getResponseDelay())
+                            .DownloadFilePart(URI, finalLeft_pos, finalRight_pos);
+                    updateProgressChecked(12, thread_id);
 
-                        byte[] file_part = response.inputStream().readAllBytes();
-                        synchronized (file_bytes) {
-                            System.arraycopy(file_part, 0, file_bytes, finalLeft_pos, file_part.length);
-                        }
-                    } catch (URISyntaxException e) {
-                        e.printStackTrace();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
+                    //Can be optimised.
+                    byte[] file_part = response.inputStream().readAllBytes();
+                    updateProgressChecked(30, thread_id);
+
+                    //+1 for correct progress division.
+                    int progress = 30, inc = file_part.length / (100 - 30) + 1;
+                    for (int i = 0; i < file_part.length; ++i) {
+                        //We don't need synchronisation because we write data to
+                        file_bytes[finalLeft_pos + i] = file_part[i];
+                        if(i % inc == 0)
+                            updateProgressChecked(++progress, thread_id);
                     }
+                    updateProgressChecked(100, thread_id);
+                } catch (URISyntaxException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
             });
 
@@ -69,18 +82,31 @@ public class MultiThreadDownloader implements WebDownloadClient {
 
             left_pos = right_pos + 1;
             right_pos += part_size;
-            if(bytes_len - right_pos <= part_size)
+            if (bytes_len - right_pos <= part_size)
                 right_pos = bytes_len - 1;
         }
 
         for (var th : _threads_pool) th.join();
 
-        try(var os = Files.newOutputStream(Paths.get(file_path))){
+        try (var os = Files.newOutputStream(Paths.get(file_path))) {
             os.write(file_bytes);
             os.flush();
         }
 
 
         return true;
+    }
+
+    private void updateProgressChecked(int n, int thread_idx) {
+        if (_progress_updater != null)
+            _progress_updater.apply(new AbstractMap.SimpleEntry<>(thread_idx, n));
+    }
+
+    public Function GetProgressUpdater() {
+        return _progress_updater;
+    }
+
+    public void SetProgressUpdater(Function _progress_updater) {
+        this._progress_updater = _progress_updater;
     }
 }
